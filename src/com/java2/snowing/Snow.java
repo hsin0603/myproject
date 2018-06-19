75e39338156cd554dc240075ea06756b0646c693
hsin0603 <gemini880603@gmail.com>
2018-06-19 14:33:07 +0800

    ChatServer

--- src/com/java2/snowing/Snow.java
+++ src/com/java2/snowing/Snow.java
@@ -1,25 +1,37 @@
 package com.java2.snowing;
 
+import java.nio.channels.ShutdownChannelGroupException;
 import java.util.Random;
+
+import com.java2.snowing.SnowFrame.SnowCanvas;
 
 public class Snow extends Thread {
 	Random random = new Random();
 	int x;
 	int y = 800;
+	private final SnowFrame.SnowCanvas snowCanvas;
 
-	public Snow() {
-		x = random.nextInt(600);
+	public Snow(SnowFrame.SnowCanvas snowCanvas) {
+		this.snowCanvas = SnowCanvas;
+		x = random.nextInt(800);
+		y = random.nextInt(600);
 	}
 
 	@Override
 	public void run() {
 		for(int i = 800 ; i>0; i--){
 //			System.out.println(getName()+":("+x+","+y+")");
-			y=y-1;
-			int movement = random.nextInt(3)-1;
-			x= x-movement;
-			System.out.print(new String(new char[x]).replace("\0"," "));
-			System.out.println("雪");
+			y++;
+			x = x + random.nextInt(3)-1;
+			snowCanvas.repaint();
+			try{
+				sleep(50);
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+//			x= x-movement;
+//			System.out.print(new String(new char[x]).replace("\0"," "));
+//			System.out.println("雪");
 			
 }
 }
