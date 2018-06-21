6c203b6127ec60d1a397001ec7ec39dc0be8c15b
hsin0603 <gemini880603@gmail.com>
2018-06-21 15:47:10 +0800

    Schedule ok

--- src/com/java2/schedule/Main.java
+++ src/com/java2/schedule/Main.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import org.ietf.jgss.GSSCredential;
+
 import com.java2.oo.Drink;
 
 /*有一文字檔儲存課表資料(schedule.txt)
@@ -25,55 +27,50 @@ public class Main {
 	List<Course> courses = new ArrayList<>();
 
 	public Main() {
-
-		try {
-			readCourses();
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("請輸入星期(1-7):");
-			int weekday = scanner.nextInt();
-			System.out.println("請輸入時(0-24):");
-			int time = scanner.nextInt();
-			boolean avail = true;
-			for (Course c : courses) {
-				if (weekday == c.weekday) {
-					if (c.time >= time && time>= (c.time+c.hour)) {
-						System.out.println("有課");
-					}
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		readCourses();
 
 	}
 
-	public void on() {
-		System.out.println("Please insert coin...");
-		for (int i = 0; i < courses.size(); i++) {
-			Course drink = courses.get(i);
-			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getTime());
-		}
-	}
-
-	public void readCourses() throws IOException {
+	public void readCourses() {
 		FileReader fr;
 		try {
 			fr = new FileReader("schedule.txt");
 			BufferedReader in = new BufferedReader(fr);
-			String line = in.readLine();
-			String[] tokens = line.split(",");
-			String id = tokens[0];
-			String name = tokens[1];
-			int weekday = Integer.parseInt(tokens[2]);
-			int time = Integer.parseInt(tokens[3]);
-			int hour = Integer.parseInt(tokens[4]);
+			String line;
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("請輸入星期(1-7):");
+			int w = scanner.nextInt();
+			System.out.println("請輸入時(0-24):");
+			int t = scanner.nextInt();
+			for (int i = 0; i < 5; i++) {
+				line = in.readLine();
+				String[] tokens = line.split(",");
+				String id = tokens[0];
+				String name = tokens[1];
+				int weekday = Integer.parseInt(tokens[2]);
+				int time = Integer.parseInt(tokens[3]);
+				int hour = Integer.parseInt(tokens[4]);
+				Course c = new Course(id, name, weekday, time, hour);
+				if (c.weekday == w) {
+					if (c.time <= t && t <= (c.time + c.hour)) {
+						System.out.println("[有課:" + id + " " + name + " 到" + (time + hour) + "時]");
+					    break;
+					} else {
+						System.out.println("[有空檔]");
+						break;
+					}
+				}else {}
+			}
+
 		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public static void main(String[] args) {
 		Main m = new Main();
-		
+
 	}
 }
